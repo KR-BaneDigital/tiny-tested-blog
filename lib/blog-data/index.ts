f10a814ef02b79b29972a2af126eeae0dfc7db5b
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-29 13:02:22 -0500

    [Auto-Publish] Index: say-goodbye-to-sore-nipples-lansinoh-nipple-cream-review

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -3,9 +3,11 @@
 
 import { post as aveeno_baby_wash_the_gentle_cleanse_for_your_little_one_s_de } from './aveeno-baby-wash-the-gentle-cleanse-for-your-little-one-s-de.data'
 import { post as evenflo_travel_system_your_ultimate_companion_for_stress_fre } from './evenflo-travel-system-your-ultimate-companion-for-stress-fre.data'
+import { post as say_goodbye_to_sore_nipples_lansinoh_nipple_cream_review } from './say-goodbye-to-sore-nipples-lansinoh-nipple-cream-review.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  say_goodbye_to_sore_nipples_lansinoh_nipple_cream_review,
   evenflo_travel_system_your_ultimate_companion_for_stress_fre,
   aveeno_baby_wash_the_gentle_cleanse_for_your_little_one_s_de
 ]
