6ca999ae8ca499b52564d21ac8ef27a0ca5bcce5
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-29 17:02:17 -0500

    [Auto-Publish] Index: numnum-spoons-set-the-ultimate-transition-tool-for-tiny-eate

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -7,9 +7,11 @@ import { post as say_goodbye_to_sore_nipples_lansinoh_nipple_cream_review } from
 import { post as sensitive_skin_savior_why_pampers_wipes_are_a_must_have } from './sensitive-skin-savior-why-pampers-wipes-are-a-must-have.data'
 import { post as why_mama_bear_refills_are_the_eco_friendly_game_changer_you_ } from './why-mama-bear-refills-are-the-eco-friendly-game-changer-you-.data'
 import { post as burp_cloths_that_save_the_day_mess_free_feedings_made_easy } from './burp-cloths-that-save-the-day-mess-free-feedings-made-easy.data'
+import { post as numnum_spoons_set_the_ultimate_transition_tool_for_tiny_eate } from './numnum-spoons-set-the-ultimate-transition-tool-for-tiny-eate.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  numnum_spoons_set_the_ultimate_transition_tool_for_tiny_eate,
   burp_cloths_that_save_the_day_mess_free_feedings_made_easy,
   why_mama_bear_refills_are_the_eco_friendly_game_changer_you_,
   sensitive_skin_savior_why_pampers_wipes_are_a_must_have,
