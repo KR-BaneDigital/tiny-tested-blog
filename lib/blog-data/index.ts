ec3a8ea69a84cec630ca2f10fee5de2b070ca1d8
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-29 16:02:12 -0500

    [Auto-Publish] Index: burp-cloths-that-save-the-day-mess-free-feedings-made-easy

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -6,9 +6,11 @@ import { post as evenflo_travel_system_your_ultimate_companion_for_stress_fre } 
 import { post as say_goodbye_to_sore_nipples_lansinoh_nipple_cream_review } from './say-goodbye-to-sore-nipples-lansinoh-nipple-cream-review.data'
 import { post as sensitive_skin_savior_why_pampers_wipes_are_a_must_have } from './sensitive-skin-savior-why-pampers-wipes-are-a-must-have.data'
 import { post as why_mama_bear_refills_are_the_eco_friendly_game_changer_you_ } from './why-mama-bear-refills-are-the-eco-friendly-game-changer-you-.data'
+import { post as burp_cloths_that_save_the_day_mess_free_feedings_made_easy } from './burp-cloths-that-save-the-day-mess-free-feedings-made-easy.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  burp_cloths_that_save_the_day_mess_free_feedings_made_easy,
   why_mama_bear_refills_are_the_eco_friendly_game_changer_you_,
   sensitive_skin_savior_why_pampers_wipes_are_a_must_have,
   say_goodbye_to_sore_nipples_lansinoh_nipple_cream_review,
