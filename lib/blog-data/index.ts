557ab3a00401a56fcfb19ce68aa3e28acda9173a
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-29 15:01:51 -0500

    [Auto-Publish] Index: why-mama-bear-refills-are-the-eco-friendly-game-changer-you-

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -5,9 +5,11 @@ import { post as aveeno_baby_wash_the_gentle_cleanse_for_your_little_one_s_de } 
 import { post as evenflo_travel_system_your_ultimate_companion_for_stress_fre } from './evenflo-travel-system-your-ultimate-companion-for-stress-fre.data'
 import { post as say_goodbye_to_sore_nipples_lansinoh_nipple_cream_review } from './say-goodbye-to-sore-nipples-lansinoh-nipple-cream-review.data'
 import { post as sensitive_skin_savior_why_pampers_wipes_are_a_must_have } from './sensitive-skin-savior-why-pampers-wipes-are-a-must-have.data'
+import { post as why_mama_bear_refills_are_the_eco_friendly_game_changer_you_ } from './why-mama-bear-refills-are-the-eco-friendly-game-changer-you-.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  why_mama_bear_refills_are_the_eco_friendly_game_changer_you_,
   sensitive_skin_savior_why_pampers_wipes_are_a_must_have,
   say_goodbye_to_sore_nipples_lansinoh_nipple_cream_review,
   evenflo_travel_system_your_ultimate_companion_for_stress_fre,
