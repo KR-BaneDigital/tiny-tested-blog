540aab930fb76aed3908aae27e9f95aa7aebf47e
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-30 03:01:56 -0500

    [Auto-Publish] Index: comotomo-baby-bottle-the-game-changer-for-stress-free-feedin

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -17,9 +17,11 @@ import { post as say_goodbye_to_stinky_diaper_days_ubbi_diaper_pail_review } fro
 import { post as birdseye_cloth_diapers_the_eco_friendly_change_every_parent_ } from './birdseye-cloth-diapers-the-eco-friendly-change-every-parent-.data'
 import { post as mess_free_mealtime_magic_bumkins_sleeved_bib_review } from './mess-free-mealtime-magic-bumkins-sleeved-bib-review.data'
 import { post as unleash_comfort_the_nursing_bras_set_redefining_motherhood_e } from './unleash-comfort-the-nursing-bras-set-redefining-motherhood-e.data'
+import { post as comotomo_baby_bottle_the_game_changer_for_stress_free_feedin } from './comotomo-baby-bottle-the-game-changer-for-stress-free-feedin.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  comotomo_baby_bottle_the_game_changer_for_stress_free_feedin,
   unleash_comfort_the_nursing_bras_set_redefining_motherhood_e,
   mess_free_mealtime_magic_bumkins_sleeved_bib_review,
   birdseye_cloth_diapers_the_eco_friendly_change_every_parent_,
