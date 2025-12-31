3af587b567e464225cadd62641d171c51ba36edd
Kyle Richardson <kyle@banedigital.com>
2025-12-30 22:28:40 -0500

    Fix Twitter metadata og:image - remove double URL concatenation

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -50,13 +50,18 @@ export async function generateMetadata({ params }: { params: Promise<{ slug: str
       type: "article",
       publishedTime: post.date,
       authors: [post.author.name],
-      images: [post.image],
+    images: post.image ? [{
+      url: post.image,
+      width: 1200,
+      height: 630,
+      alt: post.title,
+    }] : undefined,
     },
     twitter: {
       card: "summary_large_image",
       title: post.title,
       description: post.excerpt,
-      images: [post.image],
+      images: post.image ? [post.image] : undefined,
     },
   }
 }
