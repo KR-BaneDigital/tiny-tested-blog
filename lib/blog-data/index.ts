2902f84c9d5a7a174e9cbf9f3cc526efb4d97cd5
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-31 04:02:08 -0500

    [Auto-Publish] Index: soft-on-baby-easy-on-the-planet-cloth-diapers-pack-review

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -39,9 +39,11 @@ import { post as stay_confident_and_dry_my_honest_review_of_poise_pads } from '.
 import { post as peace_of_mind_why_infant_optics_monitor_is_the_ultimate_baby } from './peace-of-mind-why-infant-optics-monitor-is-the-ultimate-baby.data'
 import { post as travel_smart_with_chicco_bravo_the_ultimate_parenting_game_c } from './travel-smart-with-chicco-bravo-the-ultimate-parenting-game-c.data'
 import { post as elevate_your_nursing_journey_the_pillow_that_transforms_feed } from './elevate-your-nursing-journey-the-pillow-that-transforms-feed.data'
+import { post as soft_on_baby_easy_on_the_planet_cloth_diapers_pack_review } from './soft-on-baby-easy-on-the-planet-cloth-diapers-pack-review.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  soft_on_baby_easy_on_the_planet_cloth_diapers_pack_review,
   elevate_your_nursing_journey_the_pillow_that_transforms_feed,
   travel_smart_with_chicco_bravo_the_ultimate_parenting_game_c,
   peace_of_mind_why_infant_optics_monitor_is_the_ultimate_baby,
