272589e4f4bbe023390e124a3e3ba55799ee9d56
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-31 09:02:12 -0500

    [Auto-Publish] Index: prevail-underwear-comfort-and-confidence-for-every-day-wear

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -44,9 +44,11 @@ import { post as embrace_comfort_and_style_the_ultimate_nursing_cover_review } f
 import { post as mastering_nursing_in_style_the_breastfeeding_boss_cover_revi } from './mastering-nursing-in-style-the-breastfeeding-boss-cover-revi.data'
 import { post as lemme_purr_gummies_your_sweet_secret_to_stress_free_serenity } from './lemme-purr-gummies-your-sweet-secret-to-stress-free-serenity.data'
 import { post as unlock_radiance_discover_the_magic_of_glimmering_skin_serum } from './unlock-radiance-discover-the-magic-of-glimmering-skin-serum.data'
+import { post as prevail_underwear_comfort_and_confidence_for_every_day_wear } from './prevail-underwear-comfort-and-confidence-for-every-day-wear.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  prevail_underwear_comfort_and_confidence_for_every_day_wear,
   unlock_radiance_discover_the_magic_of_glimmering_skin_serum,
   lemme_purr_gummies_your_sweet_secret_to_stress_free_serenity,
   mastering_nursing_in_style_the_breastfeeding_boss_cover_revi,
