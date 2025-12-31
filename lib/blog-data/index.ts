c4b164e752c22c996d8fefa145f7990718a6759b
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2025-12-31 10:02:12 -0500

    [Auto-Publish] Index: slow-feeder-bowl-the-secret-to-savoring-every-bite-with-your

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -45,9 +45,11 @@ import { post as mastering_nursing_in_style_the_breastfeeding_boss_cover_revi } 
 import { post as lemme_purr_gummies_your_sweet_secret_to_stress_free_serenity } from './lemme-purr-gummies-your-sweet-secret-to-stress-free-serenity.data'
 import { post as unlock_radiance_discover_the_magic_of_glimmering_skin_serum } from './unlock-radiance-discover-the-magic-of-glimmering-skin-serum.data'
 import { post as prevail_underwear_comfort_and_confidence_for_every_day_wear } from './prevail-underwear-comfort-and-confidence-for-every-day-wear.data'
+import { post as slow_feeder_bowl_the_secret_to_savoring_every_bite_with_your } from './slow-feeder-bowl-the-secret-to-savoring-every-bite-with-your.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  slow_feeder_bowl_the_secret_to_savoring_every_bite_with_your,
   prevail_underwear_comfort_and_confidence_for_every_day_wear,
   unlock_radiance_discover_the_magic_of_glimmering_skin_serum,
   lemme_purr_gummies_your_sweet_secret_to_stress_free_serenity,
